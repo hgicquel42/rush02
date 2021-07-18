5239744ae68f31a4e8d49afe54193c9e3d625fa7
Hugo Gicquel <hugogicquel@me.com>
2021-07-18 17:41:15 +0200

    9999

--- ex00/ft_convert.c
+++ ex00/ft_convert.c
@@ -11,14 +11,48 @@ int	ft_is_tens(char	*s, char m)
 	return (0);
 }
 
-int	ft_convert_exact(t_keyvalue *dict, char *arg, int size)
+int	ft_convert_exact(t_keyvalue *dict, char c)
 {
 	t_keyvalue	*kv;
 
 	kv = dict;
 	while (kv)
 	{
-		if (kv->size == size && !ft_strncmp(kv->key, arg, size - 1))
+		if (kv->key[0] == c)
+		{
+			ft_putout(kv->value);
+			return (1);
+		}
+		kv = kv->next;
+	}
+	return (0);
+}
+
+int	ft_convert_tens(t_keyvalue *dict, char *arg)
+{
+	t_keyvalue	*kv;
+
+	kv = dict;
+	while (kv)
+	{
+		if (kv->size == 2 && ft_is_tens(kv->key, arg[0]))
+		{
+			ft_putout(kv->value);
+			return (1);
+		}
+		kv = kv->next;
+	}
+	return (0);
+}
+
+int	ft_convert_hundred(t_keyvalue *dict, int size)
+{
+	t_keyvalue	*kv;
+
+	kv = dict;
+	while (kv)
+	{
+		if (kv->size == size && ft_is_tens(kv->key, '1'))
 		{
 			ft_putout(kv->value);
 			return (1);
@@ -30,36 +64,37 @@ int	ft_convert_exact(t_keyvalue *dict, char *arg, int size)
 
 int	ft_convert2(t_keyvalue *dict, char *arg, int size)
 {
-	t_keyvalue	*kv;
-
 	if (!size)
 		return (0);
-	if (ft_convert_exact(dict, arg, size))
-		return (1);
-	kv = dict;
-	while (kv)
+	if (size == 1)
+		return (ft_convert_exact(dict, arg[0]));
+	if (size == 2)
 	{
-		if (kv->size == size && ft_is_tens(kv->key, arg[0]))
+		if (arg[0] != '0')
 		{
-			ft_putout(kv->value);
-			ft_putout(" ");
-			return (ft_convert2(dict, &(arg[kv->size - 1]), size - (kv->size - 1)));
-		}
-		kv = kv->next;
-	}
-	kv = dict;
-	while (kv)
-	{
-		if (kv->size == size && ft_is_tens(kv->key, '1'))
-		{
-			if (!ft_convert_exact(dict, arg, 1))
+			if (!ft_convert_tens(dict, arg))
 				return (0);
 			ft_putout(" ");
-			ft_putout(kv->value);
-			ft_putout(" ");
-			return (ft_convert2(dict, &(arg[kv->size - 1]), size - (kv->size - 1)));
 		}
-		kv = kv->next;
+		if (arg[1] != '0')
+		{
+			if (!ft_convert_exact(dict, arg[1]))
+				return (0);
+		}
+		return (1);
+	}
+	if (size >= 3)
+	{
+		if (arg[0] != '0')
+		{
+			if (!ft_convert_exact(dict, arg[0]))
+				return (0);
+			ft_putout(" ");
+			if (!ft_convert_hundred(dict, size))
+				return (0);
+			ft_putout(" ");
+		}
+		return (ft_convert2(dict, &arg[1], size - 1));
 	}
 	return (0);
 }
